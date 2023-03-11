02b8e1184b03c8afb96c8fb3fe13812d69dbfdd1
Alexander Hansson <alexander.hansson4@gmail.com>
2023-03-11 19:10:13 +0100

    Use accumulate because its fun

--- problems/010-summation-of-primes/main.cpp
+++ problems/010-summation-of-primes/main.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 #include <euler/primes.hpp>
 #include <vector>
-
+#include <numeric>
 int main() {
     std::vector<size_t> primes = euler_primes::get_primes(2000000);
-    size_t sum = 0;
-    for (size_t p : primes) {
-        sum+=p;
-    }
+    size_t sum = std::accumulate(primes.begin(), primes.end(), 0);
     std::cout << sum << std::endl;
 }
